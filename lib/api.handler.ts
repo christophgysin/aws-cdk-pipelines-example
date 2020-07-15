67367104fdd493603a7a384dad6985338a03050c
Christoph Gysin <christoph.gysin@gmail.com>
2020-07-15 10:17:06 +0300

    Add CORS

--- lib/api.handler.ts
+++ lib/api.handler.ts
@@ -1,10 +1,17 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 
+const {
+  ALLOWED_ORIGIN = '*',
+} = process.env;
+
 export const handler: APIGatewayProxyHandler = async (event) => {
   console.log('event:', JSON.stringify(event, null, 2));
 
   return {
     statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': ALLOWED_ORIGIN,
+    },
     body: JSON.stringify({
       message: 'Success',
     }),
