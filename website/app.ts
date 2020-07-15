6e0f85dfb89121fa8da3f1d1e585f75a3488940a
Christoph Gysin <christoph.gysin@gmail.com>
2020-07-15 18:09:01 +0300

    Bundle frontend with parcel

--- website/app.ts
+++ website/app.ts
@@ -1,9 +1,10 @@
+const apiDomain = process.env.API_DOMAIN;
+const apiUrl = `https://${apiDomain}`;
+
 (async () => {
   console.log('Hello CDK!');
 
-  const url = 'https://api.christophgys.in';
-
-  const response = await fetch(url);
+  const response = await fetch(apiUrl);
   const data = await response.json();
 
   console.log('data:', JSON.stringify(data, null, 2));
